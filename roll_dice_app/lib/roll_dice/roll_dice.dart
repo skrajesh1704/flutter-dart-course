c4069a2e3839a2444421e9a95fa297bf5ce685dc
Sk Rajesh <skrajesh.1704@gmail.com>
2026-06-10 19:51:22 +0530

    roll dice screen added

---
+++ roll_dice_app/lib/roll_dice/roll_dice.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class RollDice extends StatefulWidget {
+  const RollDice({super.key});
+
+  @override
+  State<RollDice> createState() => _RollDiceState();
+}
+
+class _RollDiceState extends State<RollDice> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [Text('Hello World!')],
+      ),
+    );
+  }
+}
