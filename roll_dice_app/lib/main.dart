4b6696eacdc5232c6439d3eb1286746a49e34707
Sk Rajesh <skrajesh.1704@gmail.com>
2026-06-10 19:52:33 +0530

    link roll dice screen to main

--- roll_dice_app/lib/main.dart
+++ roll_dice_app/lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:roll_dice_app/roll_dice/roll_dice.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: Text('Hello World!'),
+    home: RollDice(),
   ));
 }
