f788d6f0e82b21df52f55fd9fd5f0bd184a0e7fd
Sk Rajesh <skrajesh.1704@gmail.com>
2026-06-22 23:11:29 +0530

    alignment center

--- quiz_game/lib/quiz_game/quiz_screen.dart
+++ quiz_game/lib/quiz_game/quiz_screen.dart
@@ -13,16 +13,17 @@ class _QuizScreenState extends State<QuizScreen> {
     return Scaffold(
       backgroundColor: Colors.white,
       body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Padding(
-            padding: const EdgeInsets.only(top: 50.0),
-            child: Image.asset('assets/images/quiz_game.jpg'),
-          ),
+          Image.asset('assets/images/quiz_game.jpg'),
           TextButton(
             onPressed: () {},
             child: Row(
+              mainAxisSize: MainAxisSize.min,
               children: [
                 Text('Start Quiz'),
+                SizedBox(width: 10.0),
                 Icon(Icons.arrow_forward_ios_rounded, size: 15.0),
               ],
             ),
