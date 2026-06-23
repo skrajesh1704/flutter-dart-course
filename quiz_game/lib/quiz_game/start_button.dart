4d536214b7037ecf1339110ceb5b6844b6a201bb
Sk Rajesh <skrajesh.1704@gmail.com>
2026-06-23 22:59:07 +0530

    created start btn

---
+++ quiz_game/lib/quiz_game/start_button.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class StartButton extends StatelessWidget {
+  const StartButton({super.key});
+
+  @override
+  Widget build(context) {
+    return Text('BTN');
+  }
+}
