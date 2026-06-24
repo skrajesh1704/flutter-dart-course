7fc10adedeb13fed0ea5e85b4507de3f99835cbc
Sk Rajesh <skrajesh.1704@gmail.com>
2026-06-24 23:21:43 +0530

    center alignment

--- quiz_game/lib/quiz_game/question_screen.dart
+++ quiz_game/lib/quiz_game/question_screen.dart
@@ -12,14 +12,17 @@ class _QuestionScreenState extends State<QuestionScreen> {
   Widget build(context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Column(
-        children: [
-          Text('Hello, How are you...     ?'),
-          Text('Answer 1: Fine'),
-          Text('Answer 2: Doing Good'),
-          Text('Answer 3: Great'),
-          Text('Answer 4: All of the above'),
-        ],
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Hello, How are you...     ?'),
+            Text('Answer 1: Fine'),
+            Text('Answer 2: Doing Good'),
+            Text('Answer 3: Great'),
+            Text('Answer 4: All of the above'),
+          ],
+        ),
       ),
     );
   }
