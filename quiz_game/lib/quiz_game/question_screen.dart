3fe8b0bbaa1d41638068d0971df3b29c2af0d1d2
Sk Rajesh <skrajesh.1704@gmail.com>
2026-06-24 23:18:31 +0530

    question and added dummy

--- quiz_game/lib/quiz_game/question_screen.dart
+++ quiz_game/lib/quiz_game/question_screen.dart
@@ -10,6 +10,17 @@ class QuestionScreen extends StatefulWidget {
 class _QuestionScreenState extends State<QuestionScreen> {
   @override
   Widget build(context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Column(
+        children: [
+          Text('Hello, How are you...     ?'),
+          Text('Answer 1: Fine'),
+          Text('Answer 2: Doing Good'),
+          Text('Answer 3: Great'),
+          Text('Answer 4: All of the above'),
+        ],
+      ),
+    );
   }
 }
