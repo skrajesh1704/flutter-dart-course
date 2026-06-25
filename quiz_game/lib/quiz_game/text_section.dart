c51b3bbeb1f6b07e93d47b1f10a30eb150b59e2f
Sk Rajesh <skrajesh.1704@gmail.com>
2026-06-26 01:19:18 +0530

    cmt for text section

--- quiz_game/lib/quiz_game/text_section.dart
+++ quiz_game/lib/quiz_game/text_section.dart
@@ -1,13 +1,24 @@
 import 'package:flutter/material.dart';
 
+/// A reusable text widget for displaying questions and answers
 class TextSection extends StatelessWidget {
+  // Text to be displayed
   final String textInput;
+
+  // Font size of the text
   final double textSize;
+
+  // Font weight of the text
   final FontWeight textWeight;
+
   const TextSection({
     super.key,
     required this.textInput,
+
+    // Default font size
     this.textSize = 18.0,
+
+    // Default font weight
     this.textWeight = FontWeight.w400,
   });
 
