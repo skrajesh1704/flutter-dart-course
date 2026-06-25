9d8a6daa45f0fe99670525df7786966ab5be7b21
Sk Rajesh <skrajesh.1704@gmail.com>
2026-06-26 01:17:44 +0530

    cmt for quiz screen

--- quiz_game/lib/quiz_game/quiz_screen.dart
+++ quiz_game/lib/quiz_game/quiz_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:quiz_game/quiz_game/start_button.dart';
 
+/// Main quiz screen widget
 class QuizScreen extends StatefulWidget {
   const QuizScreen({super.key});
 
@@ -8,15 +9,29 @@ class QuizScreen extends StatefulWidget {
   State<QuizScreen> createState() => _QuizScreenState();
 }
 
+/// State class for QuizScreen
 class _QuizScreenState extends State<QuizScreen> {
   @override
-  Widget build(context) {
+  Widget build(BuildContext context) {
     return Scaffold(
+      // Sets the screen background color
       backgroundColor: Colors.white,
+
+      // Main content of the screen
       body: Column(
+        // Centers children horizontally
         crossAxisAlignment: CrossAxisAlignment.center,
+
+        // Centers children vertically
         mainAxisAlignment: MainAxisAlignment.center,
-        children: [Image.asset('assets/images/quiz_game.jpg'), StartButton()],
+
+        children: [
+          // Displays the quiz image from assets
+          Image.asset('assets/images/quiz_game.jpg'),
+
+          // Custom button widget to start the quiz
+          StartButton(),
+        ],
       ),
     );
   }
