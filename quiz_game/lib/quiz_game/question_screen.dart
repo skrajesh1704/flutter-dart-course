cb5f2418595742400cd8011cd88b637074b6b424
Sk Rajesh <skrajesh.1704@gmail.com>
2026-06-28 23:14:51 +0530

    cmt based on the new data imp

--- quiz_game/lib/quiz_game/question_screen.dart
+++ quiz_game/lib/quiz_game/question_screen.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:quiz_game/quiz_game/dummy_quiz_data.dart';
 import 'package:quiz_game/quiz_game/text_section.dart';
 
-/// Screen that displays a quiz question and its answer options
+/// Screen that displays a quiz question and its answer options.
 class QuestionScreen extends StatefulWidget {
   const QuestionScreen({super.key});
 
@@ -10,58 +10,64 @@ class QuestionScreen extends StatefulWidget {
   State<QuestionScreen> createState() => _QuestionScreenState();
 }
 
-/// State class for QuestionScreen
+/// State class that manages the current quiz question.
 class _QuestionScreenState extends State<QuestionScreen> {
+  /// Stores the index of the currently displayed question.
   int currentQuestion = 0;
+
   @override
   Widget build(BuildContext context) {
+    /// Retrieves the current question from the quiz data.
     final questionSet = questionData[currentQuestion];
+
     return Scaffold(
-      // Sets the background color of the screen
+      /// Sets the screen background color.
       backgroundColor: Colors.white,
 
       body: Center(
         child: Column(
-          // Centers all widgets vertically
+          /// Centers all widgets vertically.
           mainAxisAlignment: MainAxisAlignment.center,
 
           children: [
-            // Displays the quiz question
+            /// Displays the current question.
             TextSection(
               textInput: questionSet['question'],
               textSize: 22.0,
               textWeight: FontWeight.w500,
             ),
 
+            /// Dynamically generates a TextSection widget
+            /// for each answer option in the current question.
             ...List.generate(questionSet['options'].length, (index) {
-              return TextSection(textInput: questionSet["options"][index]);
+              return TextSection(textInput: questionSet['options'][index]);
             }),
 
+            /// Button to move to the next question.
             ElevatedButton(
               onPressed: () {
+                /// Checks if there are more questions remaining.
                 if (currentQuestion < questionData.length - 1) {
                   setState(() {
+                    /// Moves to the next question.
                     currentQuestion++;
                   });
                 } else {
                   setState(() {
+                    /// Restarts the quiz after the last question.
                     currentQuestion = 0;
                   });
                 }
               },
-              child: Text('Next'),
+              child: const Text('Next'),
             ),
 
-            // // Answer option 1
+            // Example static answer widgets (not used because
+            // answers are generated dynamically above).
+
             // TextSection(textInput: 'Answer 1: Fine'),
-
-            // // Answer option 2
             // TextSection(textInput: 'Answer 2: Doing Good'),
-
-            // // Answer option 3
             // TextSection(textInput: 'Answer 3: Great'),
-
-            // // Answer option 4
             // TextSection(textInput: 'Answer 4: All of the above'),
           ],
         ),
