673fe9e4a33d7db82e58ef871ff130e4a0656ccc
Sk Rajesh <skrajesh.1704@gmail.com>
2026-06-29 23:45:13 +0530

    added cmts

--- quiz_game/lib/quiz_game/next_btn.dart
+++ quiz_game/lib/quiz_game/next_btn.dart
@@ -2,7 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:quiz_game/quiz_game/dummy_quiz_data.dart';
 
 class NextBtn extends StatefulWidget {
+  // Index of the currently displayed question.
   final int currentQuestion;
+
+  // Callback to notify the parent widget when the question changes.
   final ValueChanged<int> onQuestionChanged;
 
   const NextBtn({
@@ -16,29 +19,37 @@ class NextBtn extends StatefulWidget {
 }
 
 class _NextBtnState extends State<NextBtn> {
+  // Moves to the next question.
   void nextQuestion() {
     int nextIndex;
 
+    // If there are more questions, move to the next one.
     if (widget.currentQuestion < questionData.length - 1) {
       nextIndex = widget.currentQuestion + 1;
     } else {
+      // If the last question is reached, start again from the first question.
       nextIndex = 0;
     }
 
+    // Send the updated question index back to the parent widget.
     widget.onQuestionChanged(nextIndex);
   }
 
   @override
   Widget build(BuildContext context) {
     return ElevatedButton(
+      // Called when the button is pressed.
       onPressed: nextQuestion,
-      style: ElevatedButton.styleFrom(
-        backgroundColor: Colors.transparent, // Button color
-        foregroundColor: Colors.black, // Text color
 
+      // Styles the button.
+      style: ElevatedButton.styleFrom(
+        backgroundColor: Colors.transparent,
+        foregroundColor: Colors.black,
         shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
         textStyle: const TextStyle(fontSize: 18, fontWeight: FontWeight.w400),
       ),
+
+      // Text displayed on the button.
       child: const Text('Next'),
     );
   }
