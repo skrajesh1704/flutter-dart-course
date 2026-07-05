11f61173253c1ba0bc4cefcb98f3a550c062943b
Sk Rajesh <skrajesh.1704@gmail.com>
2026-07-05 23:42:24 +0530

    fw for nxt btn

--- quiz_game/lib/quiz_game/next_btn.dart
+++ quiz_game/lib/quiz_game/next_btn.dart
@@ -58,7 +58,7 @@ class _NextBtnState extends State<NextBtn> {
         'Next',
         style: TextStyle(
           fontSize: 18.0,
-          fontWeight: FontWeight.w400,
+          fontWeight: FontWeight.w500,
           color: Colors.black,
         ),
       ),
